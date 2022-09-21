ced2914e8c6d11ed8c4eb355a43cd173bd0f5167
iubns <iubns@naver.com>
2022-09-21 22:00:02 +0900

    New: client - server alias 세팅

--- client/pages/admin/all-user.tsx
+++ client/pages/admin/all-user.tsx
@@ -1,13 +1,12 @@
 import { useEffect, useState } from "react";
 import { Stack } from "@mui/material";
+import { User } from '@entity/user'
 
 function AllUser () {
-
-    const [allUserList, setAllUserList] = useState([])
+    const [allUserList, setAllUserList] = useState([] as Array<User>)
 
 
     useEffect(() => {
-
 
         fetch('/api/get/all-user')
         .then(
@@ -24,12 +23,7 @@ function AllUser () {
             {allUserList.map(user => {
                 return (
                     <Stack direction="row">
-                        <Stack>
-                            {user.name}
-                        </Stack>
-                        <Stack>
-                            {user.age}
-                        </Stack>
+                        {user.name} {user.age} {user.id} {user.phone} {user.sex}
                     </Stack>
                 )
             })}
