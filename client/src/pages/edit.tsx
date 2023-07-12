412e648015df69fa97cbf9c17e0cd33ed645ed18
iubns <iubns@naver.com>
2023-07-12 22:29:41 +0900

    update: 계좌 복사 가능하도록 수정

--- client/src/pages/edit.tsx
+++ client/src/pages/edit.tsx
@@ -37,18 +37,24 @@ export default function Edit () {
                 입금 처리가 {userData.deposit ? '완료 되었습' : '진행 중입'}니다.
                 </Stack>:
                 <Stack
-                maxWidth="400px"
-                padding="5px"
-                marginTop="10px"
-                bgcolor="#03C0A4"
-                borderRadius="5px"
-                textAlign="center"
-                alignItems="center"
-                justifyContent="center"
-                border="1px solid 008000"
+                    maxWidth="400px"
+                    padding="5px"
+                    marginTop="10px"
+                    bgcolor="#03C0A4"
+                    borderRadius="5px"
+                    textAlign="center"
+                    alignItems="center"
+                    justifyContent="center"
+                    border="1px solid 008000"
                 >
                     <Stack fontSize="20px" fontWeight="bold" color="whitesmoke">수련회비 안내</Stack>
-                    <Stack color="white" mt="6px"> 3333276342153 카카오뱅크
+                    <Stack 
+                        color="white" 
+                        mt="6px"
+                        style={{
+                            userSelect: 'text'
+                        }}
+                    > 3333276342153 카카오뱅크 (조영래)
                     <br/>전참 5만 / 금요일 저녁 이후 참석 4만</Stack>
                 </Stack>}
             <UserInformationForm
