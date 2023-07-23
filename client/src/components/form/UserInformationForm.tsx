bb036d116f4a076635220d0994fef296dced46a4
iubns <iubns@naver.com>
2023-07-23 11:26:48 +0900

    update: 카풀 중복접수 수정

--- client/src/components/form/UserInformationForm.tsx
+++ client/src/components/form/UserInformationForm.tsx
@@ -1,197 +1,201 @@
-import { Button, 
-  MenuItem, 
+import {
+  Button,
+  MenuItem,
   InputLabel,
-  Select, 
+  Select,
   FormControl,
-  TextField, } from "@mui/material";
-import { Stack } from "@mui/system";
-import { InOutInfo } from "@entity/inOutInfo";
-import { User } from "@entity/user";
-import { post } from "../../pages/api";
-import { useEffect, useState } from "react";
-import { AttendType } from "@entity/types";
-import InOutFrom from "./InOutForm";
-import { NotificationMessage } from "state/notification";
-import { useSetRecoilState } from "recoil";
-import { HowToGo, MoveType } from "types";
+  TextField,
+} from "@mui/material"
+import { Stack } from "@mui/system"
+import { InOutInfo } from "@entity/inOutInfo"
+import { User } from "@entity/user"
+import { post } from "../../pages/api"
+import { useEffect, useState } from "react"
+import { AttendType } from "@entity/types"
+import InOutFrom from "./InOutForm"
+import { NotificationMessage } from "state/notification"
+import { useSetRecoilState } from "recoil"
+import { HowToGo, MoveType } from "types"
 interface IProps {
-    user: User
-    inOutData: Array<InOutInfo>
+  user: User
+  inOutData: Array<InOutInfo>
+  checkToken: () => void
 }
 
-export default function UserInformationForm (props: IProps) {
-    const [userInformation, setUserInformation] = useState(new User())
-    const [inOutData, setInOutData] = useState<Array<InOutInfo>>([])
-    const [showItems, setShowItems] = useState(false)
-    const setNotificationMessage = useSetRecoilState(NotificationMessage)
+export default function UserInformationForm(props: IProps) {
+  const [userInformation, setUserInformation] = useState(new User())
+  const [inOutData, setInOutData] = useState<Array<InOutInfo>>([])
+  const [showItems, setShowItems] = useState(false)
+  const setNotificationMessage = useSetRecoilState(NotificationMessage)
 
-    useEffect(() => {
-      setUserInformation(props.user || {} as User)
-      setInOutData(props.inOutData || {} as Array<InOutInfo>)
-      if(props.user && props.user.id){
-        setShowItems(true)
-      }
-    },[props.user])
+  useEffect(() => {
+    setShowItems(false)
+    setUserInformation(props.user || ({} as User))
+    setInOutData(props.inOutData || ({} as Array<InOutInfo>))
+    if (props.user && props.user.id) {
+      setShowItems(true)
+    }
+  }, [props.user])
 
-    const changeInformation = (type: string, data: string) => {
-      setUserInformation({...userInformation, [type]: data})
+  const changeInformation = (type: string, data: string) => {
+    setUserInformation({ ...userInformation, [type]: data })
+  }
+
+  const submit = async () => {
+    if (!userInformation.name) {
+      setNotificationMessage("이름을 입력해주세요.")
+      return
+    } else if (!userInformation.age) {
+      setNotificationMessage("나이를 입력해주세요.")
+      return
+    } else if (!userInformation.attendType) {
+      setNotificationMessage("전참 여부를 선택해주세요.")
+      return
+    } else if (!userInformation.sex) {
+      setNotificationMessage("성별을 선택해주세요.")
+      return
+    } else if (!userInformation.phone) {
+      setNotificationMessage("전화번호를 입력해주세요.")
+      return
+    } else if (
+      userInformation.attendType === AttendType.full &&
+      !userInformation.howToGo
+    ) {
+      setNotificationMessage("이동 방법을 선택해주세요.")
+      return
     }
 
-    const submit = async () => {
-        if(!userInformation.name){
-          setNotificationMessage('이름을 입력해주세요.')
-          return
-        }else if(!userInformation.age){
-          setNotificationMessage('나이를 입력해주세요.')
-          return
-        }else if(!userInformation.attendType){
-          setNotificationMessage('전참 여부를 선택해주세요.')
-          return
-        }else if(!userInformation.sex){
-          setNotificationMessage('성별을 선택해주세요.')
-          return
-        }else if(!userInformation.phone){
-          setNotificationMessage('전화번호를 입력해주세요.')
-          return
-        }else if(userInformation.attendType === AttendType.full && !userInformation.howToGo){
-          setNotificationMessage('이동 방법을 선택해주세요.')
-          return
-        }
+    const url = "/auth/edit-user"
 
-        const url = '/auth/edit-user'
-
-        const saveResult = await post(url, userInformation)
-        let attendTimeResult;
-          // @ts-ignore 
-          if(inOutData.length > 0 && userInformation.howToGo !== HowToGo.together){
-            attendTimeResult = await post('/info/save-attend-time', {
-              userId: saveResult.userId,
-              inOutData,
-            })
-          }
-
-          if(saveResult.result === "success"){
-            localStorage.setItem('token', saveResult.token)
-          }else{
-            setNotificationMessage('접수중 오류가 발생하였습니다.\n다시 시도해주세요.')
-            return
-          }
-
-          if(attendTimeResult && attendTimeResult.result !== "success"){
-            setNotificationMessage('참가 일정 내역 저장중에 문제가 발생하였습니다.\n시간, 장소. 이동방법을 모두 입력해주세요.')
-            return
-          }
-          setNotificationMessage(`신청 내역이 저장이 되었습니다.`)
+    const saveResult = await post(url, userInformation)
+    let attendTimeResult
+    // @ts-ignore
+    if (inOutData.length > 0 && userInformation.howToGo !== HowToGo.together) {
+      attendTimeResult = await post("/info/save-attend-time", {
+        userId: saveResult.userId,
+        inOutData,
+      })
     }
 
-    function getInputGap(){
-      return <Stack margin="6px" />
+    if (saveResult.result === "success") {
+      localStorage.setItem("token", saveResult.token)
+    } else {
+      setNotificationMessage(
+        "접수중 오류가 발생하였습니다.\n다시 시도해주세요."
+      )
+      return
     }
 
-    function getLabelGap(){
-      return <Stack margin="2px" />
+    if (attendTimeResult && attendTimeResult.result !== "success") {
+      setNotificationMessage(
+        "참가 일정 내역 저장중에 문제가 발생하였습니다.\n시간, 장소. 이동방법을 모두 입력해주세요."
+      )
+      return
     }
+    setNotificationMessage(`신청 내역이 저장이 되었습니다.`)
+    props.checkToken()
+  }
 
-    return (<Stack
-        padding="6px"
-        minWidth="360px"
-      >
-        <Stack>
-          {getInputGap()}
-          <Stack>
-            이름
-          </Stack>
-          {getLabelGap()}
-          <TextField
-              fullWidth={true}
-              value={userInformation.name}
-              placeholder="이름을 입력하세요."
-              onChange={e => changeInformation("name", e.target.value)}
-            />
-        </Stack>
+  function getInputGap() {
+    return <Stack margin="6px" />
+  }
+
+  function getLabelGap() {
+    return <Stack margin="2px" />
+  }
+
+  return (
+    <Stack padding="6px" minWidth="360px">
+      <Stack>
         {getInputGap()}
-        <Stack>
-          <Stack 
-            width="80px"
-            justifyContent="center"
-          >
-            나이
-          </Stack>
-          {getLabelGap()}
-          <TextField
-            type="number"
-            fullWidth={true}
-            placeholder="나이를 입력하세요."
-            value={userInformation.age}
-            onChange={e => changeInformation("age", e.target.value)}
-          />
-          </Stack>
-          {getInputGap()}
-          <Stack>
-          <Stack 
-            width="80px"
-            justifyContent="center"
-          >
-            성별
-          </Stack>
-          {getLabelGap()}
-          <FormControl
-            fullWidth={true}>
-            {// @ts-ignore 
-              showItems &&
+        <Stack>이름</Stack>
+        {getLabelGap()}
+        <TextField
+          fullWidth={true}
+          value={userInformation.name}
+          placeholder="이름을 입력하세요."
+          onChange={(e) => changeInformation("name", e.target.value)}
+        />
+      </Stack>
+      {getInputGap()}
+      <Stack>
+        <Stack width="80px" justifyContent="center">
+          나이
+        </Stack>
+        {getLabelGap()}
+        <TextField
+          type="number"
+          fullWidth={true}
+          placeholder="나이를 입력하세요."
+          value={userInformation.age}
+          onChange={(e) => changeInformation("age", e.target.value)}
+        />
+      </Stack>
+      {getInputGap()}
+      <Stack>
+        <Stack width="80px" justifyContent="center">
+          성별
+        </Stack>
+        {getLabelGap()}
+        <FormControl fullWidth={true}>
+          {
+            // @ts-ignore
+            showItems && (
               <Select
                 value={userInformation.sex}
                 defaultValue={userInformation.sex}
                 placeholder="성별을 선택하세요."
-                onChange={e => changeInformation("sex", e.target.value)}
-                >
-                <MenuItem value={'man'}>
-                  남
-                </MenuItem>
-                <MenuItem value={'woman'}>
-                  여
-                </MenuItem>
-              </Select>}
-          </FormControl>
-        </Stack>
-        {getInputGap()}
-        <Stack>
-          {// @ts-ignore 
-            showItems &&
-          <Stack >
-            <Stack minWidth="100px" justifyContent="center">
-              전참 / 부참
+                onChange={(e) => changeInformation("sex", e.target.value)}
+              >
+                <MenuItem value={"man"}>남</MenuItem>
+                <MenuItem value={"woman"}>여</MenuItem>
+              </Select>
+            )
+          }
+        </FormControl>
+      </Stack>
+      {getInputGap()}
+      <Stack>
+        {
+          // @ts-ignore
+          showItems && (
+            <Stack>
+              <Stack minWidth="100px" justifyContent="center">
+                전참 / 부참
+              </Stack>
+              {getLabelGap()}
+              <Select
+                fullWidth={true}
+                value={userInformation.attendType}
+                defaultValue={userInformation.attendType}
+                placeholder="참여 시간을 선택하세요."
+                onChange={(e) =>
+                  changeInformation("attendType", e.target.value.toString())
+                }
+              >
+                <MenuItem value={AttendType.full}>전참</MenuItem>
+                <MenuItem value={AttendType.half}>부분 참석</MenuItem>
+              </Select>
             </Stack>
-            {getLabelGap()}
-            <Select
-              fullWidth={true}
-              value={userInformation.attendType}
-              defaultValue={userInformation.attendType}
-              placeholder="참여 시간을 선택하세요."
-              onChange={e => changeInformation("attendType", e.target.value.toString())}
-            >
-              <MenuItem value={AttendType.full}>
-                전참
-              </MenuItem>
-              <MenuItem value={AttendType.half}>
-                부분 참석
-              </MenuItem>
-            </Select>
-          </Stack>}
-          {// @ts-ignore 
-            userInformation.attendType === AttendType.full &&
+          )
+        }
+        {
+          // @ts-ignore
+          userInformation.attendType === AttendType.full && (
             <Stack ml="10px">
               {getInputGap()}
-              <Stack minWidth="100px" justifyContent="center" >
+              <Stack minWidth="100px" justifyContent="center">
                 이동 방법
               </Stack>
               {getLabelGap()}
-              <Select 
+              <Select
                 fullWidth={true}
                 defaultValue={userInformation.howToGo}
                 value={userInformation.howToGo}
-                onChange={e => changeInformation("howToGo", e.target.value.toString())}
-              >  
+                onChange={(e) =>
+                  changeInformation("howToGo", e.target.value.toString())
+                }
+              >
                 <MenuItem value={HowToGo.together.toString()}>
                   교회 버스로만 이동
                 </MenuItem>
@@ -200,58 +204,51 @@ export default function UserInformationForm (props: IProps) {
                 </MenuItem>
               </Select>
             </Stack>
-          }
-          {// @ts-ignore 
-            (userInformation.howToGo === HowToGo.car.toString() || userInformation.attendType === AttendType.half )&&
+          )
+        }
+        {
+          // @ts-ignore
+          (userInformation.howToGo === HowToGo.car.toString() ||
+            userInformation.attendType === AttendType.half) && (
             <Stack ml="10px">
-              <InOutFrom
-                setInOutData={setInOutData}
-                inOutData={inOutData}
-              />
+              <InOutFrom setInOutData={setInOutData} inOutData={inOutData} />
             </Stack>
-          }
+          )
+        }
+      </Stack>
+      {getInputGap()}
+      <Stack>
+        <Stack width="80px" justifyContent="center">
+          전화번호
         </Stack>
-        {getInputGap()}
-        <Stack>
-          <Stack 
-            width="80px"
-            justifyContent="center"
-          >
-            전화번호
-          </Stack>
-          {getLabelGap()}
-          <TextField
-            fullWidth={true}
-            value={userInformation.phone}
-            placeholder="전화번호를 입력하세요."
-            onChange={e => changeInformation("phone", e.target.value)}
-            />
+        {getLabelGap()}
+        <TextField
+          fullWidth={true}
+          value={userInformation.phone}
+          placeholder="전화번호를 입력하세요."
+          onChange={(e) => changeInformation("phone", e.target.value)}
+        />
+      </Stack>
+      {getInputGap()}
+      <Stack>
+        <Stack width="80px" justifyContent="center">
+          기타사항
         </Stack>
-        {getInputGap()}
-        <Stack >
-          <Stack 
-            width="80px"
-            justifyContent="center"
-          >
-            기타사항
-          </Stack>
-          {getLabelGap()}
-          <TextField
-            fullWidth={true}
-            value={userInformation.etc}
-            placeholder="기타사항이 있을 경우 입력하세요."
-            onChange={e => changeInformation("etc", e.target.value)}
-          />
-        </Stack>
-        {getInputGap()}
-          
-        <Stack marginTop="10px">
-          <Button
-              variant="contained"
-              onClick={submit}
-            >
-                저장
-            </Button> 
-        </Stack>
-    </Stack>)
+        {getLabelGap()}
+        <TextField
+          fullWidth={true}
+          value={userInformation.etc}
+          placeholder="기타사항이 있을 경우 입력하세요."
+          onChange={(e) => changeInformation("etc", e.target.value)}
+        />
+      </Stack>
+      {getInputGap()}
+
+      <Stack marginTop="10px">
+        <Button variant="contained" onClick={submit}>
+          저장
+        </Button>
+      </Stack>
+    </Stack>
+  )
 }
--- client/src/pages/edit.tsx
+++ client/src/pages/edit.tsx
@@ -1,75 +1,84 @@
-import { Stack } from "@mui/system";
-import { useEffect, useState } from "react";
-import { post } from "./api";
+import { Stack } from "@mui/system"
+import { useEffect, useState } from "react"
+import { post } from "./api"
 import { User } from "@entity/user"
-import UserInformationForm from "../components/form/UserInformationForm";
-import { InOutInfo } from "@entity/inOutInfo";
+import UserInformationForm from "../components/form/UserInformationForm"
+import { InOutInfo } from "@entity/inOutInfo"
 
-export default function Edit () {
-    const [userData, setUserData] = useState({} as User)
-    const [inOutData, setInOutData] = useState<Array<InOutInfo>>([])
-    
-    useEffect(() => {
-        const token = localStorage.getItem('token')
-        if(token){
-            checkToken(token);
-        }
-    }, [])
+export default function Edit() {
+  const [userData, setUserData] = useState({} as User)
+  const [inOutData, setInOutData] = useState<Array<InOutInfo>>([])
 
-    const checkToken = (token: string) => {
-        post('/auth/check-token', {
-            token,
-        }).then(response => {
-            if(response.result === "true"){
-                setUserData(response.userData)
-                setInOutData(response.inoutInfoList)
-            }
-        })
+  useEffect(() => {
+    checkToken()
+  }, [])
+
+  const checkToken = () => {
+    const token = localStorage.getItem("token")
+    if (!token) {
+      return
     }
-    
-    return (
+    post("/auth/check-token", {
+      token,
+    }).then((response) => {
+      if (response.result === "true") {
+        setUserData(response.userData)
+        setInOutData(response.inoutInfoList)
+      }
+    })
+  }
+
+  return (
     <Stack padding="12px">
-        <Stack
+      <Stack alignItems="center" justifyContent="center">
+        {userData.deposit ? (
+          <Stack
+            px="24px"
+            py="12px"
+            fontWeight="500"
+            color="#099"
+            style={{
+              borderRadius: "12px",
+              border: "solid #AAA 2px",
+            }}
+          >
+            입금 처리가 {userData.deposit ? "완료 되었습" : "진행 중입"}니다.
+          </Stack>
+        ) : (
+          <Stack
+            maxWidth="400px"
+            padding="5px"
+            marginTop="10px"
+            bgcolor="#03C0A4"
+            borderRadius="5px"
+            textAlign="center"
             alignItems="center"
             justifyContent="center"
-        >
-            {userData.deposit ? <Stack
-                px="24px"
-                py="12px"
-                fontWeight="500"
-                color="#099"
-                style={{
-                    borderRadius: '12px',
-                    border: 'solid #AAA 2px',
-                }}
+            border="1px solid 008000"
+          >
+            <Stack fontSize="20px" fontWeight="bold" color="whitesmoke">
+              수련회비 안내
+            </Stack>
+            <Stack
+              color="white"
+              mt="6px"
+              style={{
+                userSelect: "text",
+              }}
             >
-                입금 처리가 {userData.deposit ? '완료 되었습' : '진행 중입'}니다.
-                </Stack>:
-                <Stack
-                    maxWidth="400px"
-                    padding="5px"
-                    marginTop="10px"
-                    bgcolor="#03C0A4"
-                    borderRadius="5px"
-                    textAlign="center"
-                    alignItems="center"
-                    justifyContent="center"
-                    border="1px solid 008000"
-                >
-                    <Stack fontSize="20px" fontWeight="bold" color="whitesmoke">수련회비 안내</Stack>
-                    <Stack 
-                        color="white" 
-                        mt="6px"
-                        style={{
-                            userSelect: 'text'
-                        }}
-                    > 3333276342153 카카오뱅크 (조영래)
-                    <br/>전체 5만</Stack>
-                </Stack>}
-            <UserInformationForm
-                user={userData}
-                inOutData={inOutData}
-             />
-        </Stack>
-    </Stack>)
+              {" "}
+              3333276342153 카카오뱅크 (조영래)
+              <br />
+              전체 5만
+            </Stack>
+          </Stack>
+        )}
+        <UserInformationForm
+          user={userData}
+          inOutData={inOutData}
+          checkToken={checkToken}
+        />
+      </Stack>
+    </Stack>
+  )
 }
