401bc307d655e2630586a8c530e3fc7af974ca17
iubns <iubns@naver.com>
2023-08-02 11:26:06 +0900

    update: 카풀 보기 쉽도록 변경 및 조배정 나이순 정렬

--- client/src/pages/admin/carpooling.tsx
+++ client/src/pages/admin/carpooling.tsx
@@ -1,216 +1,235 @@
-import { InOutInfo } from "@entity/inOutInfo";
-import { Days, MoveType , InOutType} from "../../types";
-import { get, post } from "../../pages/api";
-import { useEffect, useState } from "react";
-import { Stack } from "@mui/system";
-import { Box, MenuItem, Select } from "@mui/material";
-import { User } from "@entity/user";
-import { useRouter } from "next/router";
-import { useSetRecoilState } from "recoil";
-import { NotificationMessage } from "state/notification";
-
+import { InOutInfo } from "@entity/inOutInfo"
+import { Days, MoveType, InOutType } from "../../types"
+import { get, post } from "../../pages/api"
+import { useEffect, useState } from "react"
+import { Stack } from "@mui/system"
+import { Box, MenuItem, Select } from "@mui/material"
+import { User } from "@entity/user"
+import { useRouter } from "next/router"
+import { useSetRecoilState } from "recoil"
+import { NotificationMessage } from "state/notification"
 
 function Carpooling() {
-    const router = useRouter()
-    const setNotificationMessage = useSetRecoilState(NotificationMessage)
-    const [carList, setCarList] = useState([] as InOutInfo[])
-    const [rideUserList, setRideUserList] = useState([] as InOutInfo[])
-    const [selectedInfo, setSelectedInfo] = useState({} as InOutInfo)
+  const router = useRouter()
+  const setNotificationMessage = useSetRecoilState(NotificationMessage)
+  const [carList, setCarList] = useState([] as InOutInfo[])
+  const [rideUserList, setRideUserList] = useState([] as InOutInfo[])
+  const [selectedInfo, setSelectedInfo] = useState({} as InOutInfo)
 
-    const [selectedDay, setSelectedDay] = useState<Number>(Days.firstDay)
-    const [selectedInOut, setSelectedInOut] = useState<string>(InOutType.IN)
-    const [mousePoint, setMousePoint] = useState([0,0])
-    const [isShowUserInfo, setIsShowUserInfo] = useState(false)
-    const [showUserInfo, setShowUserInfo] = useState({} as User)
+  const [selectedDay, setSelectedDay] = useState<Number>(Days.firstDay)
+  const [selectedInOut, setSelectedInOut] = useState<string>(InOutType.IN)
+  const [mousePoint, setMousePoint] = useState([0, 0])
+  const [isShowUserInfo, setIsShowUserInfo] = useState(false)
+  const [showUserInfo, setShowUserInfo] = useState({} as User)
 
-    function onMouseMove(event: MouseEvent) {
-        setMousePoint([event.pageX, event.pageY])
+  function onMouseMove(event: MouseEvent) {
+    setMousePoint([event.pageX, event.pageY])
+  }
+
+  useEffect(() => {
+    fetchData()
+    addEventListener("mousemove", onMouseMove)
+
+    return () => {
+      removeEventListener("mousemove", onMouseMove)
     }
+  }, [])
 
+  function modal() {
+    if (!isShowUserInfo) {
+      return <Stack />
+    }
+    return (
+      <Stack
+        style={{
+          position: "absolute",
+          top: mousePoint[1] + 10,
+          left: mousePoint[0] + 10,
+          border: "1px solid #CCC",
+          borderRadius: "12px",
+          padding: "4px",
+          backgroundColor: "white",
+        }}
+      >
+        {showUserInfo.etc}
+        <Stack height="1px" bgcolor="#DDD" my="4px" />
+        {showUserInfo.phone}
+      </Stack>
+    )
+  }
 
-    useEffect(() => {
-        fetchData()
-        addEventListener('mousemove', onMouseMove);
+  async function setCar(car: InOutInfo) {
+    selectedInfo.rideCarInfo = car
+    await post("/admin/set-car", {
+      inOutInfo: selectedInfo,
+    })
+    fetchData()
+  }
 
-        return () => {
-            removeEventListener('mousemove', onMouseMove)
+  async function setEmptyCar() {
+    selectedInfo.rideCarInfo = null
+    await post("/admin/set-car", {
+      inOutInfo: selectedInfo,
+    })
+    fetchData()
+  }
+
+  function fetchData() {
+    get("/admin/get-car-info")
+      .then((data: InOutInfo[]) => {
+        // @ts-ignore
+        if (data.error) {
+          router.push("/admin")
+          setNotificationMessage("권한이 없습니다.")
+          return
         }
-    }, [])
 
-    function modal(){
-        if(!isShowUserInfo){
-            return <Stack/>
-        }
-        return(
+        // @ts-ignore
+        const cars = data.filter(
+          (info) => info.howToMove === MoveType.driveCarWithPerson
+        )
+        setCarList(cars)
+        // @ts-ignore
+        const rideUsers = data.filter(
+          (info) => info.howToMove === MoveType.rideCar && !info.rideCarInfo
+        )
+        setRideUserList(rideUsers)
+      })
+      .catch((response) => {
+        console.log(response)
+      })
+  }
+
+  function setModal(user: User) {
+    setIsShowUserInfo(true)
+    setShowUserInfo(user)
+  }
+
+  function getRowOfInfo(info: InOutInfo) {
+    return (
+      <Stack
+        my="4px"
+        borderRadius="4px"
+        direction="column"
+        border="1px solid #ACACAC"
+        onMouseEnter={() => {
+          setModal(info.user)
+        }}
+        onMouseLeave={() => {
+          setIsShowUserInfo(false)
+        }}
+        onMouseDown={() => setSelectedInfo(info)}
+      >
         <Stack
-            style={{
-                position: 'absolute',
-                top: mousePoint[1] + 10,
-                left: mousePoint[0] + 10,
-                border: '1px solid #CCC',
-                borderRadius: '12px',
-                padding: '4px',
-                backgroundColor: 'white',
-            }}
+          direction="row"
+          alignItems="center"
+          justifyContent="space-evenly"
         >
-            {showUserInfo.etc}
-            <Stack height="1px" bgcolor="#DDD" my="4px"/>
-            {showUserInfo.phone}
-        </Stack>)
-    }
+          <Box>{info.user?.name}</Box> <Box>{info.position}</Box>
+        </Stack>
+        <Box textAlign="center">{info.time}</Box>
+      </Stack>
+    )
+  }
 
-    async function setCar(car: InOutInfo){
-        selectedInfo.rideCarInfo = car
-        await post('/admin/set-car', {
-            inOutInfo: selectedInfo
-        })
-        fetchData();
-    }
-
-    async function setEmptyCar(){
-        selectedInfo.rideCarInfo = null
-        await post('/admin/set-car', {
-            inOutInfo: selectedInfo
-        })
-        fetchData();
-    }
-
-    function fetchData(){
-        get('/admin/get-car-info')
-        .then((data: InOutInfo[]) => {
-            // @ts-ignore
-            if(data.error){
-                router.push('/admin')
-                setNotificationMessage("권한이 없습니다.")
-                return
-            }
-
-            // @ts-ignore
-            const cars = data.filter(info => info.howToMove === MoveType.driveCarWithPerson)
-            setCarList(cars)
-            // @ts-ignore
-            const rideUsers = data.filter(info => info.howToMove === MoveType.rideCar && !info.rideCarInfo)
-            setRideUserList(rideUsers)
-        })
-        .catch(response => {
-            console.log(response)
-        })
-    }
-
-    function setModal(user: User){
-        setIsShowUserInfo(true)
-        setShowUserInfo(user)
-    }
-
-    function getRowOfInfo(info: InOutInfo){
-        return (<Stack
-                direction="row"
+  return (
+    <Stack direction="row">
+      <Stack
+        style={{
+          margin: "12px",
+        }}
+      >
+        <Select
+          style={{
+            marginBottom: "8px",
+          }}
+          value={selectedDay}
+          onChange={(e) => setSelectedDay(e.target.value as number)}
+        >
+          <MenuItem value={Days.firstDay}>첫째날</MenuItem>
+          <MenuItem value={Days.secondDay}>둘째날</MenuItem>
+          <MenuItem value={Days.thirdDay}>셋째날</MenuItem>
+        </Select>
+        <Select
+          value={selectedInOut}
+          onChange={(e) => setSelectedInOut(e.target.value as string)}
+        >
+          <MenuItem value={InOutType.IN}>들어가는 차</MenuItem>
+          <MenuItem value={InOutType.OUT}>나가는 차</MenuItem>
+        </Select>
+      </Stack>
+      {modal()}
+      <Stack
+        style={{
+          margin: "8px",
+          padding: "4px",
+          minWidth: "240px",
+          borderRadius: "8px",
+          paddingBottom: "20px",
+          border: "1px solid #ACACAC",
+          boxShadow: "2px 2px 5px 3px #ACACAC;",
+        }}
+        onMouseUp={setEmptyCar}
+      >
+        <Stack textAlign="center">탑승 예정자</Stack>
+        {rideUserList
+          .filter(
+            (info) =>
+              info.day === selectedDay && info.inOutType === selectedInOut
+          )
+          .map((info) => getRowOfInfo(info))}
+      </Stack>
+      <Stack
+        style={{
+          margin: "4px",
+          display: "flex",
+          flexWrap: "wrap",
+          flexDirection: "row",
+          width: "calc(100% - 200px)",
+        }}
+        direction="row"
+      >
+        {carList
+          .filter(
+            (info) =>
+              info.day === selectedDay && info.inOutType === selectedInOut
+          )
+          .map((car) => (
+            <Stack
+              sx={{
+                margin: "8px",
+                padding: "4px",
+                minWidth: "240px",
+                borderRadius: "8px",
+                border: "1px solid #ACACAC",
+                boxShadow: "2px 2px 5px 3px #ACACAC;",
+              }}
+              onMouseUp={() => setCar(car)}
+            >
+              <Stack
                 justifyContent="space-evenly"
+                textAlign="center"
                 onMouseEnter={() => {
-                    setModal(info.user)
+                  setModal(car.user)
                 }}
                 onMouseLeave={() => {
-                    setIsShowUserInfo(false)
+                  setIsShowUserInfo(false)
                 }}
-                onMouseDown={() => setSelectedInfo(info)
-            }
-        >
-            <Box>{info.user?.name}</Box>  <Box>{info.time}</Box> <Box>{info.position}</Box>
-        </Stack>)
-    }
-
-    return(<Stack direction="row">
-        <Stack
-            style={{
-                margin: '12px'
-            }}>
-            <Select
-                style={{
-                    marginBottom: '8px'
-                }}
-                value={selectedDay}
-                onChange={e => setSelectedDay(e.target.value as number)}
-            >
-                <MenuItem value={Days.firstDay}>
-                    첫째날
-                </MenuItem>
-                <MenuItem value={Days.secondDay}>
-                    둘째날
-                </MenuItem>
-                <MenuItem value={Days.thirdDay}>
-                    셋째날
-                </MenuItem>
-            </Select>
-            <Select 
-            value={selectedInOut}
-            onChange={e => setSelectedInOut(e.target.value as string)}
-            >
-                <MenuItem value={InOutType.IN}>
-                    들어가는 차
-                </MenuItem>
-                <MenuItem value={InOutType.OUT}>
-                    나가는 차
-                </MenuItem>
-            </Select>
-        </Stack>
-        {modal()}
-        <Stack
-            style={{
-                margin: '8px',
-                padding: '4px',
-                minWidth: '240px',
-                borderRadius: '8px',
-                paddingBottom: '20px',
-                border: "1px solid #ACACAC",
-                boxShadow: '2px 2px 5px 3px #ACACAC;',
-            }}
-            onMouseUp={setEmptyCar}
-        >
-            <Stack textAlign="center">탑승 예정자</Stack>
-            {rideUserList.filter(info => info.day === selectedDay && info.inOutType === selectedInOut).map(info => getRowOfInfo(info))}
-        </Stack>
-        <Stack 
-            style={{
-                margin: '4px',
-                display: 'flex',
-                flexWrap: 'wrap',
-                flexDirection: "row",
-                width: 'calc(100% - 200px)'
-            }} 
-            direction="row"
-        >
-        {carList.filter(info => info.day === selectedDay && info.inOutType === selectedInOut).map(car => 
-            <Stack
-            sx={{
-                margin: '8px',
-                padding: '4px',
-                minWidth: '240px',
-                borderRadius: '8px',
-                border: "1px solid #ACACAC",
-                boxShadow: '2px 2px 5px 3px #ACACAC;',
-            }}
-            onMouseUp={() => setCar(car)}
-            >
-                <Stack
-                justifyContent="space-evenly"
-                    textAlign="center"
-                    onMouseEnter={() => {
-                        setModal(car.user)
-                    }}
-                    onMouseLeave={() => {
-                        setIsShowUserInfo(false)
-                    }}
-                    direction="row"
-                >
-                    {car.user.name}의 차 
-                    <Box>{car.time}</Box>
-                    <Box>{car.position}</Box>
+                direction="column"
+              >
+                <Stack direction="row" justifyContent="space-evenly">
+                  <Box>{car.user.name}의 차</Box>
+                  <Box>{car.position}</Box>
                 </Stack>
-                <Box height="1px" bgcolor="#DDD" my="4px"/>
-                {car.userInTheCar.map(info => getRowOfInfo(info))}
-            </Stack>)}
-        </Stack>
-    </Stack>)
+                <Box>{car.time}</Box>
+              </Stack>
+              <Box height="1px" bgcolor="#DDD" my="4px" />
+              {car.userInTheCar.map((info) => getRowOfInfo(info))}
+            </Stack>
+          ))}
+      </Stack>
+    </Stack>
+  )
 }
 
-export default Carpooling;
+export default Carpooling
--- client/src/pages/admin/group-formation.tsx
+++ client/src/pages/admin/group-formation.tsx
@@ -1,202 +1,237 @@
-import { Box, Stack } from "@mui/material";
-import { User } from "@entity/user";
-import { useEffect, useState } from "react";
-import { get, post } from "../../pages/api";
-import { InOutInfo } from "@entity/inOutInfo";
-import { AttendType } from "@entity/types";
+import { Box, Stack } from "@mui/material"
+import { User } from "@entity/user"
+import { useEffect, useState } from "react"
+import { get, post } from "../../pages/api"
+import { InOutInfo } from "@entity/inOutInfo"
+import { AttendType } from "@entity/types"
 
+function GroupFormation() {
+  const [unassignedUserList, setUnassignedUserList] = useState(
+    [] as Array<User>
+  )
+  const [groupList, setGroupList] = useState([] as Array<Array<User>>)
+  const [selectedUser, setSelectedUser] = useState<User>()
+  const [mousePoint, setMousePoint] = useState([0, 0])
+  const [maxGroupNumber, setMaxGroupNumber] = useState(0)
+  const [isShowUserInfo, setIsShowUserInfo] = useState(false)
+  const [showUserInfo, setShowUserInfo] = useState({} as User)
+  const [userAttendInfoCache, setUserAttendInfoCache] = useState(
+    [] as Array<Array<InOutInfo>>
+  )
+  const [userAttendInfo, setUserAttendInfo] = useState([] as Array<InOutInfo>)
 
-function GroupFormation (){
-    const [unassignedUserList, setUnassignedUserList] = useState([] as Array<User>)
-    const [groupList, setGroupList] = useState([] as Array<Array<User>>)
-    const [selectedUser, setSelectedUser] = useState<User>()
-    const [mousePoint, setMousePoint] = useState([0,0])
-    const [maxGroupNumber, setmaxGroupNumber] = useState(0)
-    const [isShowUserInfo, setIsShowUserInfo] = useState(false)
-    const [showUserInfo, setShowUserInfo] = useState({} as User)
-    const [userAttendInfoCache, setUserAttendInfoCache] = useState([] as Array<Array<InOutInfo>>)
-    const [userAttendInfo, setUserAttendInfo] = useState([] as Array<InOutInfo>)
+  function onMouseMove(event: MouseEvent) {
+    setMousePoint([event.pageX, event.pageY])
+  }
 
-    function onMouseMove(event: MouseEvent) {
-        setMousePoint([event.pageX, event.pageY])
+  useEffect(() => {
+    fetchData()
+    addEventListener("mousemove", onMouseMove)
+
+    return () => {
+      removeEventListener("mousemove", onMouseMove)
     }
+  }, [])
 
-    useEffect(() => {
-        fetchData()
-        addEventListener('mousemove', onMouseMove);
+  function fetchData() {
+    get("/admin/get-group-formation").then((response: Array<User>) => {
+      const unassignedUserList = response
+        .filter((user) => user.groupAssignment.groupNumber === 0)
+        .sort((a, b) => a.age - b.age)
+      setUnassignedUserList(unassignedUserList)
 
-        return () => {
-            removeEventListener('mousemove', onMouseMove)
+      const group = [] as Array<Array<User>>
+      const assignedUserList = response.filter(
+        (user) => user.groupAssignment.groupNumber !== 0
+      )
+      assignedUserList.map((user) => {
+        const groupNumber = user.groupAssignment.groupNumber - 1
+        if (!group[groupNumber]) {
+          group[groupNumber] = [user]
+        } else {
+          group[groupNumber].push(user)
         }
-    }, [])
+        setGroupList(group)
+      })
+      const maxNumer = Math.max(
+        ...response.map((user) => user.groupAssignment.groupNumber)
+      )
+      setMaxGroupNumber(maxNumer)
+    })
+  }
 
-    function fetchData(){
-        get('/admin/get-group-formation')
-        .then((response: Array<User>) => {
-            const unassignedUserList = response.filter(user => user.groupAssignment.groupNumber === 0)
-            setUnassignedUserList(unassignedUserList)
+  function unassignedUserRow(user: User) {
+    return (
+      <Stack
+        direction="row"
+        onMouseDown={() => setSelectedUser(user)}
+        onMouseUp={() => setGroup(0)}
+        onMouseEnter={() => {
+          setModal(user)
+        }}
+        onMouseLeave={() => {
+          setIsShowUserInfo(false)
+        }}
+        sx={{
+          justifyContent: "space-between",
+          backgroundColor: user.sex === "man" ? "lightblue" : "pink",
+        }}
+        px="4px"
+      >
+        <Box>
+          {user.name}({user.age}) (
+          {user.attendType === AttendType.full ? "전" : "부"})
+          {user.etc || (user.inOutInfos && user.inOutInfos.length) > 0
+            ? "*"
+            : ""}
+        </Box>
+        <Box>{user.groupAssignment.groupNumber}</Box>
+      </Stack>
+    )
+  }
 
-            const group = [] as Array<Array<User>>
-            const assignedUserList = response.filter(user => user.groupAssignment.groupNumber !== 0)
-            assignedUserList.map(user => {
-                const groupNumber = user.groupAssignment.groupNumber - 1
-                if(!group[groupNumber]){
-                    group[groupNumber] = [user]
-                }else{
-                    group[groupNumber].push(user)
-                }
-                setGroupList(group)
-            })
-            const maxNumer = Math.max(...response.map(user => user.groupAssignment.groupNumber))
-            setmaxGroupNumber(maxNumer)
-        })
+  function userGroupRow(user: User) {
+    return (
+      <Stack
+        direction="row"
+        p="2px"
+        onMouseDown={() => setSelectedUser(user)}
+        width="160px"
+        onMouseEnter={() => {
+          setModal(user)
+        }}
+        onMouseLeave={() => {
+          setIsShowUserInfo(false)
+        }}
+        sx={{
+          justifyContent: "space-between",
+          backgroundColor: user.sex === "man" ? "lightblue" : "pink",
+        }}
+      >
+        <Box>
+          {user.name}({user.age}) (
+          {user.attendType === AttendType.full ? "전" : "부"})
+          {user.etc || (user.inOutInfos && user.inOutInfos.length) > 0
+            ? "*"
+            : ""}
+        </Box>
+      </Stack>
+    )
+  }
+
+  function setModal(user: User) {
+    setIsShowUserInfo(true)
+    setShowUserInfo(user)
+    setUserAttendInfo(user.inOutInfos)
+  }
+
+  function Group(groupNumber: number, userList: Array<User>) {
+    return (
+      <Stack
+        sx={{
+          margin: "8px",
+          minHeight: "20px",
+          borderRadius: "8px",
+          border: "1px solid #ACACAC",
+          boxShadow: "2px 2px 5px 3px #ACACAC;",
+        }}
+        onMouseUp={() => setGroup(groupNumber)}
+      >
+        <Stack width="160px" textAlign="center" py="4px">
+          {groupNumber}조 ({userList.length})
+        </Stack>
+        {userList.map((user) => userGroupRow(user))}
+      </Stack>
+    )
+  }
+
+  async function setGroup(groupNumber: number) {
+    if (!selectedUser) {
+      return
+    }
+    selectedUser.groupAssignment.groupNumber = groupNumber
+    await post("/admin/set-group", {
+      groupAssignment: selectedUser.groupAssignment,
+    })
+    fetchData()
+  }
+
+  function modal() {
+    if (!isShowUserInfo) {
+      return <Stack />
     }
 
-    function unassignedUserRow(user: User) {
-        return (<Stack 
-            direction="row"
-            onMouseDown={() => setSelectedUser(user)}
-            onMouseUp={() => setGroup(0)}
-            onMouseEnter={() => {
-                setModal(user)
-            }}
-            onMouseLeave={() => {
-                setIsShowUserInfo(false)
-            }}
-            sx={{
-                justifyContent: 'space-between',
-                backgroundColor: user.sex === 'man' ? "lightblue" : "pink",
-            }}
-            px="4px"
-        >
-            <Box>{user.name}({user.age}) ({user.attendType === AttendType.full ? '전' : '부'}){user.etc || (user.inOutInfos && user.inOutInfos.length) > 0 ? "*" : ''}</Box>
-            <Box>{user.groupAssignment.groupNumber}</Box>
-        </Stack>)
-    }
-
-    function userGroupRow(user: User){
-        return (<Stack
-            direction="row"
-            p="2px"
-            onMouseDown={() => setSelectedUser(user)}
-            width="160px"
-            onMouseEnter={() => {
-                setModal(user)
-            }}
-            onMouseLeave={() => {
-                setIsShowUserInfo(false)
-            }}
-            sx={{
-                justifyContent: 'space-between',
-                backgroundColor: user.sex === 'man' ? "lightblue" : "pink",
-            }}
-         >
-            <Box>{user.name}({user.age}) ({user.attendType === AttendType.full ? '전' : '부'}){user.etc || (user.inOutInfos && user.inOutInfos.length) > 0 ? "*" : ''}</Box>
-        </Stack>)
-    }
-
-    function setModal(user: User){
-        setIsShowUserInfo(true)
-        setShowUserInfo(user)
-        setUserAttendInfo(user.inOutInfos)
-    }
-
-    function Group(groupNumber: number, userList: Array<User>){
-        return (<Stack
-            sx={{
-                margin: '8px',
-                minHeight: "20px",
-                borderRadius: '8px',
-                border: "1px solid #ACACAC",
-                boxShadow: '2px 2px 5px 3px #ACACAC;',
-            }}
-            onMouseUp={() => setGroup(groupNumber)}
-        >
-            <Stack width="160px" textAlign="center" py="4px">{groupNumber}조 ({userList.length})</Stack>
-            {userList.map(user => userGroupRow(user))}
-        </Stack>)
-    }
-    
-    async function setGroup(groupNumber: number){
-        if(!selectedUser){
-            return
-        }
-        selectedUser.groupAssignment.groupNumber = groupNumber
-        await post('/admin/set-group', {
-            groupAssignment: selectedUser.groupAssignment,
-        })
-        fetchData()
-    }
-
-    function modal(){
-        if(!isShowUserInfo){
-            return <Stack/>
-        }
-
-        if(!showUserInfo.etc && userAttendInfo.length === 0){
-            return <Stack />
-        }
-
-        return(
-        <Stack
-            style={{
-                padding: '6px',
-                borderRadius: '12px',
-                position: 'absolute',
-                top: mousePoint[1] + 10,
-                left: mousePoint[0] + 10,
-                backgroundColor: '#FEFEFE',
-                border: '1px solid #ACACAC',
-            }}
-        >
-            {showUserInfo.etc}
-            {showUserInfo.etc && userAttendInfo.length > 0 && <Box width="100%" height="1px" bgcolor="#ACACAC" my="4px"/>}
-            {userAttendInfo.map(info => (<Stack>{["첫", "둘", "셋"][info.day]}째 날 / {info.time} / {info.inOutType === 'in' ? '들어옴' : '나감'}</Stack>))}
-        </Stack>)
+    if (!showUserInfo.etc && userAttendInfo.length === 0) {
+      return <Stack />
     }
 
     return (
-        <Stack direction="row">
-            {modal()}
-            <Stack 
-                style={{
-                    margin: '6px',
-                    minHeight: "20px",
-                    borderRadius: '8px',
-                    paddingBottom: '20px',
-                    boxShadow: '2px 2px 5px 3px #ACACAC;',
-                    border: "1px solid #ACACAC",
-                }}
-                onMouseUp={() => setGroup(0)}
-                width="160px"
-            >
-                <Box textAlign="center" py="4px">미배정({unassignedUserList.length}명)</Box>
-                {unassignedUserList.map(user => unassignedUserRow(user))}
-            </Stack>
-            <Stack 
-                style={{
-                    flexWrap: 'wrap',
-                    margin: '4px',
-                    width: 'calc(100% - 200px)',
-                    display: 'flex',
-                    flexDirection: "row",
-                }} 
-            >
-                { new Array(maxGroupNumber).fill(0).map((_, index) => {
-                    const group = groupList[index]
-                    if(!group || group.length === 0){
-                        return Group(index + 1, [])
-                    }else{
-                        return Group(group[0].groupAssignment.groupNumber, group)
-                    }
-                })
-                }
-                {
-                    Group(maxGroupNumber + 1, [])
-                }
-            </Stack>
-        </Stack>
+      <Stack
+        style={{
+          padding: "6px",
+          borderRadius: "12px",
+          position: "absolute",
+          top: mousePoint[1] + 10,
+          left: mousePoint[0] + 10,
+          backgroundColor: "#FEFEFE",
+          border: "1px solid #ACACAC",
+        }}
+      >
+        {showUserInfo.etc}
+        {showUserInfo.etc && userAttendInfo.length > 0 && (
+          <Box width="100%" height="1px" bgcolor="#ACACAC" my="4px" />
+        )}
+        {userAttendInfo.map((info) => (
+          <Stack>
+            {["첫", "둘", "셋"][info.day]}째 날 / {info.time} /{" "}
+            {info.inOutType === "in" ? "들어옴" : "나감"}
+          </Stack>
+        ))}
+      </Stack>
     )
+  }
+
+  return (
+    <Stack direction="row">
+      {modal()}
+      <Stack
+        style={{
+          margin: "6px",
+          minHeight: "20px",
+          borderRadius: "8px",
+          paddingBottom: "20px",
+          boxShadow: "2px 2px 5px 3px #ACACAC;",
+          border: "1px solid #ACACAC",
+        }}
+        onMouseUp={() => setGroup(0)}
+        width="160px"
+      >
+        <Box textAlign="center" py="4px">
+          미배정({unassignedUserList.length}명)
+        </Box>
+        {unassignedUserList.map((user) => unassignedUserRow(user))}
+      </Stack>
+      <Stack
+        style={{
+          flexWrap: "wrap",
+          margin: "4px",
+          width: "calc(100% - 200px)",
+          display: "flex",
+          flexDirection: "row",
+        }}
+      >
+        {new Array(maxGroupNumber).fill(0).map((_, index) => {
+          const group = groupList[index].sort((a, b) => a.age - b.age)
+          if (!group || group.length === 0) {
+            return Group(index + 1, [])
+          } else {
+            return Group(group[0].groupAssignment.groupNumber, group)
+          }
+        })}
+        {Group(maxGroupNumber + 1, [])}
+      </Stack>
+    </Stack>
+  )
 }
 
 export default GroupFormation
