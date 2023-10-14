6be0cf52786ee3740034adc17fc15de4a3774bee
iubns <iubns@naver.com>
2023-10-14 22:42:56 +0900

    update: OG 수정

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,26 +1,29 @@
-import { RecoilEnv, RecoilRoot } from 'recoil'
-import '../../styles/globals.css'
+import { RecoilEnv, RecoilRoot } from "recoil"
+import "../../styles/globals.css"
 import Head from "next/head"
-import Notification from 'components/notification/notification'
+import Notification from "components/notification/notification"
 
 function MyApp({ Component, pageProps }: any) {
-  RecoilEnv.RECOIL_DUPLICATE_ATOM_KEY_CHECKING_ENABLED = false;
-  return <>
-    <Head>
-      <meta name="title" property="og:title" content="The Tipping Point" />
-      <meta
-        name="description"
-        property="og:description"
-        content="2023 새벽이슬 하계 수련회"/>
-      {/*<meta name="image" property="og:image" content="%PUBLIC_URL%/logo.png" />
+  RecoilEnv.RECOIL_DUPLICATE_ATOM_KEY_CHECKING_ENABLED = false
+  return (
+    <>
+      <Head>
+        <meta name="title" property="og:title" content="태신자 작정" />
+        <meta
+          name="description"
+          property="og:description"
+          content="태신자 작정"
+        />
+        {/*<meta name="image" property="og:image" content="%PUBLIC_URL%/logo.png" />
       <meta name="url" property="og:url" content="" />*/}
-    </Head>
-    <RecoilRoot>
-      <Notification />
-      <script src="https://developers.kakao.com/sdk/js/kakao.js"></script>
-      <Component {...pageProps} />
-  </RecoilRoot>
-  </>
+      </Head>
+      <RecoilRoot>
+        <Notification />
+        <script src="https://developers.kakao.com/sdk/js/kakao.js"></script>
+        <Component {...pageProps} />
+      </RecoilRoot>
+    </>
+  )
 }
 
 export default MyApp
