3039e8a84eae076db59040e75218339a9ceed8f5
iubns <iubns@naver.com>
2024-01-18 23:34:19 +0900

    fix: type error

--- server/src/index.ts
+++ server/src/index.ts
@@ -16,39 +16,35 @@ app.use("/", apiRouter)
 
 var server = express()
 
-var privateKey = fs.readFileSync(
-  "/etc/letsencrypt/live/{iubns.net}/privkey.pem"
-)
-var certificate = fs.readFileSync("/etc/letsencrypt/live/{iubns.net}/cert.pem")
-var ca = fs.readFileSync("/etc/letsencrypt/live/{iubns.net}/chain.pem")
+var privateKey = fs.readFileSync("/etc/letsencrypt/live/iubns.net/privkey.pem")
+var certificate = fs.readFileSync("/etc/letsencrypt/live/iubns.net/cert.pem")
+var ca = fs.readFileSync("/etc/letsencrypt/live/iubns.net/chain.pem")
 const credentials = { key: privateKey, cert: certificate, ca: ca }
 /*
 //이 부분에 router등 설정을 해주면 됩니다.
 */
 
-https
-  .createServer(credentials, server, (req, res) => {})
-  .listen(port, async () => {
-    await Promise.all([dataSource.initialize()])
-    //dataSource.dropDatabase()
-    const userList = await userDatabase.find({
-      relations: {
-        roomAssignment: true,
-        groupAssignment: true,
-        permissions: true,
-        inOutInfos: true,
-      },
-    })
-    const normalUser = userList.filter((user) =>
-      user.kakaoId.startsWith("normal")
-    )
-
-    const pro = normalUser.map(async (user) => {
-      return await deleteUser(user)
-    })
-    await Promise.all(pro)
-    console.log("start server")
+https.createServer(credentials, server).listen(port, async () => {
+  await Promise.all([dataSource.initialize()])
+  //dataSource.dropDatabase()
+  const userList = await userDatabase.find({
+    relations: {
+      roomAssignment: true,
+      groupAssignment: true,
+      permissions: true,
+      inOutInfos: true,
+    },
   })
+  const normalUser = userList.filter((user) =>
+    user.kakaoId.startsWith("normal")
+  )
+
+  const pro = normalUser.map(async (user) => {
+    return await deleteUser(user)
+  })
+  await Promise.all(pro)
+  console.log("start server")
+})
 
 app.get("/", async (req, res) => {
   res.send("running server")
